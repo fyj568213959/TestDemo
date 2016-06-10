96679d83c952395969fde13150b38efe37706e3a
fengyj <fengyj@mapbar.com>
2016-06-10 12:13:51 +0800

    修改tag

--- src/com/example/testdemo/MainActivity.java
+++ src/com/example/testdemo/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Log.i("tag", "");
+		Log.i("tag", "123");
 
 	}
 
